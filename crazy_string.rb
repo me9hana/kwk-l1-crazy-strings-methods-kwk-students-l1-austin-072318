efbf44fb1a1a29b2bdf3d5d01f4dc851e34368b9
me9hana <meghanagoli@gmail.com>
2018-07-24 19:14:21 +0000

    Done.

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,5 +1,10 @@
 # write your method here
 
-def crazy_strings(a,b)
-
+def crazy_strings(a, b)
+  newA = a.reverse.upcase
+  newB = b.swapcase.gsub("s", "z")
+  
+  "#{newA} #{newB}" # no need for puts just return it
 end
+
+crazy_strings("okay", "friends")
